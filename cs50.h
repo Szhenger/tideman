b83f2a7066b9bc650670015bc68d2c1195c60f1f
Shuoberto <115667535+Szhenger@users.noreply.github.com>
2024-08-02 11:08:47 -0400

    Update cs50.h

--- cs50.h
+++ cs50.h
@@ -1,1 +1,24 @@
-// TODO
+#ifndef CS50_H
+#define CS50_H
+
+#include <float.h>
+#include <limits.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+typedef char *string;
+
+/**
+ * Prompts user for a line of text from standard input and returns
+ * it as a string (char *), sans trailing line ending. Supports
+ * CR (\r), LF (\n), and CRLF (\r\n) as line endings. If user
+ * inputs only a line ending, returns "", not NULL. Returns NULL
+ * upon error or no input whatsoever (i.e., just EOF). Stores string
+ * on heap, but library's destructor frees memory on program's exit.
+ */
+string get_string(va_list *args, const char *format, ...) __attribute__((format(printf, 2, 3)));
+#define get_string(...) get_string(NULL, __VA_ARGS__)
+
+#endif
+

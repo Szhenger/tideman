6c8babd5a92c4090d547cf612b9b99191ded259a
Shuo <115667535+Szhenger@users.noreply.github.com>
2024-09-21 11:47:14 -0400

    Update tideman.c

--- tideman.c
+++ tideman.c
@@ -233,7 +233,6 @@ void print_winner(void)
     for (int i = 0; i < candidate_count; i++)
     {
         candidate_edges[i] = 0;
-
         for (int j = 0; j < candidate_count; j++)
         {
             if (locked[j][i])
